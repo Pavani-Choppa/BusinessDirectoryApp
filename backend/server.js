2c9177cde188a101e6446b0368dd30e889d5c6bc
Pavani-Choppa <pavanicm011@gmail.com>
2026-01-06 21:49:29 +0530

    FINAL CHANGES ARE DONE

--- backend/server.js
+++ backend/server.js
@@ -128,5 +128,5 @@ app.get('/reviews/:businessName', async (req, res) => {
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
